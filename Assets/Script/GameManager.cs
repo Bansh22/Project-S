e363d1f87ed76b40f2e996861d356f69a9f5e4db
Bansh22 <33442058+Bansh22@users.noreply.github.com>
2023-10-04 21:58:53 +0900

    config 가동중!

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -12,11 +12,16 @@ public class GameManager : MonoBehaviour
     public static GameManager instance;
     public float speed=5;
     public Player player;
-   
+
 
     private void Awake()
     {
-      
+        ConfigReader configreaders = new ConfigReader();
+        configreaders.Initialize(configreaders.GetfilePath());
+        configreaders.MakeDiction("Sap Wappon");
+
+        object rowdata = configreaders.GetDictionaryValue("damage");
+        Debug.Log(rowdata.ToString());
 
         instance = this;
         
