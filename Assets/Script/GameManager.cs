fbb810eb65c48f2e855772767823630849bd93d6
SilverTree <aprkvnem@gmail.com>
2023-10-04 14:28:55 +0900

    create enemy
    몹 유저 추적 코드와 애니메이션 제작

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -3,14 +3,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-[Serializable]
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
-    
     public Player player;
-
+    public float speed = 5;
     private void Awake()
     {
         instance = this;
--- Assets/Script/Player/Player.cs
+++ Assets/Script/Player/Player.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     Transform trans;
-    public float speed;
-    public Vector3 inputVec;
     Animator anim;
+    Rigidbody2D rigid;
+
+    float speed;
+    public Vector3 inputVec;
 
     // Start is called before the first frame update
     void Start()
@@ -16,10 +18,12 @@ public class Player : MonoBehaviour
         trans = GetComponent<Transform>();
         //sprite의 물리적 특성 (위치 크기 회전)
         anim = GetComponent<Animator>();
+        rigid = GetComponent<Rigidbody2D>();
     }
     // Update is called once per frame
     void Update()
     {
+        speed = GameManager.instance.speed;
         inputVec.x = Input.GetAxisRaw("Horizontal");
         inputVec.y = Input.GetAxisRaw("Vertical");
         //Raw의 경우 0,1로 이진값으로 바꿔주는 장치
@@ -32,6 +36,9 @@ public class Player : MonoBehaviour
         //합산된 벡터 계산, normalized의 경우 x,y 벡터의 합이 1이상이 되기때문에 1로 고정
         //fixedDeltaTime의 경우 달라지는 프레임 대비
         trans.Translate(nextVec);
+
         //위치 이동
+
+        rigid.velocity = Vector3.zero;
     }
 }
