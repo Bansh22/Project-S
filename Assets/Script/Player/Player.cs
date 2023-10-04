064e959eb98cc61e382cc3f7d0bfdd817c73162c
Bansh22 <33442058+Bansh22@users.noreply.github.com>
2023-10-04 14:26:21 +0900

    맵 일단 제작

--- Assets/Script/Player/Player.cs
+++ Assets/Script/Player/Player.cs
@@ -1,37 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
     Transform trans;
     public float speed;
-    public Vector3 inputVec;
+    public Vector2 inputVec;
     Animator anim;
 
     // Start is called before the first frame update
+    Rigidbody2D rigid;
+
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+
     void Start()
     {
 
         trans = GetComponent<Transform>();
         //sprite의 물리적 특성 (위치 크기 회전)
         anim = GetComponent<Animator>();
+       
     }
     // Update is called once per frame
-    void Update()
-    {
-        inputVec.x = Input.GetAxisRaw("Horizontal");
-        inputVec.y = Input.GetAxisRaw("Vertical");
-        //Raw의 경우 0,1로 이진값으로 바꿔주는 장치
-    }
+   
 
     private void FixedUpdate()
     {
         anim.SetFloat("Speed", inputVec.magnitude);
-        Vector3 nextVec = inputVec.normalized * speed * Time.fixedDeltaTime;
+        Vector2 nextVec = inputVec * speed * Time.fixedDeltaTime;
         //합산된 벡터 계산, normalized의 경우 x,y 벡터의 합이 1이상이 되기때문에 1로 고정
         //fixedDeltaTime의 경우 달라지는 프레임 대비
+        rigid.MovePosition(rigid.position + nextVec);
         trans.Translate(nextVec);
         //위치 이동
+    }
+
+
+    void OnMove(InputValue val){
+        inputVec = val.Get<Vector2>();
     }
 }
