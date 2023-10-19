17bafaa272b1af10631b92c0f016e7c4bfc5875c
Bansh22 <33442058+Bansh22@users.noreply.github.com>
2023-10-20 01:01:36 +0900

    업그레이드 개발완료

---
+++ Assets/Script/Player/UpgradePlayer.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradePlayer : MonoBehaviour
+{
+    private ConfigReader readerplayer;
+    private ConfigReader readerSapWappon;
+    private ConfigReader readerShootingWappon;
+    private int shootdamage;
+    private float shootspeed;
+    private float Sapdamage;
+    private int Sapcount;
+    private int playerHP;
+    private int playerSpeed;
+    // Start is called before the first frame update
+  
+
+    public  void HPupgrade()
+    {
+        readerplayer = new ConfigReader("Player");
+        playerHP = readerplayer.Search<int>("hp");
+        Debug.Log(playerHP);
+        if (playerHP < 401)
+        {
+            float temp = playerHP + 10;
+        string stringtemp = temp.ToString();
+        readerplayer.UpdateData("hp", stringtemp);
+        }
+    } 
+    public void playerspeedup()
+    {
+        readerplayer = new ConfigReader("Player");
+        playerSpeed = readerplayer.Search<int>("speed");
+        if (playerSpeed < 6)
+        {
+            float temp = playerSpeed + 1;
+            string stringtemp = temp.ToString();
+            readerplayer.UpdateData("speed", stringtemp);
+        }
+    }
+    public void sapatkup()
+    {
+        readerSapWappon = new ConfigReader("Sap Wappon");
+        Sapdamage = readerSapWappon.Search<float>("damage");
+        Debug.Log(Sapdamage);
+        if (Sapdamage < 70)
+        {
+            float temp = Sapdamage + 5;
+            string stringtemp = temp.ToString();
+            readerSapWappon.UpdateData("damage", stringtemp);
+        }
+    }
+    public void sapcountup()
+    {
+        readerSapWappon = new ConfigReader("Sap Wappon");
+        Sapcount = readerSapWappon.Search<int>("Count");
+        if (Sapcount < 6)
+        {
+            float temp = Sapcount + 1;
+            string stringtemp = temp.ToString();
+            readerSapWappon.UpdateData("Count", stringtemp);
+        }
+    }
+    public void shootspeedup()
+    {
+
+        readerShootingWappon = new ConfigReader("Shooting Wappon");
+        shootspeed = readerShootingWappon.Search<float>("speed");
+        if (shootspeed >0.3)
+        {
+            float temp = shootspeed - 0.1f;
+            string stringtemp = temp.ToString();
+            readerShootingWappon.UpdateData("speed", stringtemp);
+        }
+    }
+    public void programmermode()
+    {
+        readerplayer = new ConfigReader("Player");
+        playerHP = readerplayer.Search<int>("hp");
+      
+            float temp = 9999f;
+            string stringtemp = temp.ToString();
+            readerplayer.UpdateData("hp", stringtemp);
+       
+
+       
+    }
+
+}
